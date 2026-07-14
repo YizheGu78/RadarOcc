28609c308467812b2fcc398112264d2399cd9570
Yizhe Gu <yizhegu78@gmail.com>
2026-07-14 17:01:21 +0200

    register strict RadarOcc-S detector

--- projects/occ_plugin/occupancy/detectors/__init__.py
+++ projects/occ_plugin/occupancy/detectors/__init__.py
@@ -1,3 +1,4 @@
 from .radarocc_self import RadarDopplerAttnEASelf
 from .radarocc_self_small import RadarOcc_small
+from .radarocc_self_small_strict import RadarOcc_small_strict
 # from .radarocc_polar_tensor import RadarTensorAttn
