651dbd8d90f6b8ce25d752f1c53ab72310d21b42
Yizhe Gu <yizhegu78@gmail.com>
2026-09-21 16:09:55 +0200

    Test branch frame cleanup before reruns

--- tradition/tests/test_branch_comparison_video.py
+++ tradition/tests/test_branch_comparison_video.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from tradition.core.config import GridConfig
+from tradition.visualization import branch_comparison_video
 from tradition.visualization.branch_comparison_video import _points_to_sparse_zyx
 
 
@@ -17,3 +18,18 @@ def test_branch_points_are_voxelized_in_radarocc_zyx_order():
     coordinates = _points_to_sparse_zyx(points, GridConfig())
 
     np.testing.assert_array_equal(coordinates, [[0, 0, 0]])
+
+
+def test_branch_renderer_clears_stale_frames(tmp_path):
+    frames_dir = tmp_path / "branch_frames"
+    frames_dir.mkdir()
+    stale_frame = frames_dir / "frame_99999.png"
+    stale_frame.touch()
+
+    branch_comparison_video.BranchComparisonVideoRenderer(
+        tmp_path,
+        scene="3",
+        keep_frames=True,
+    )
+
+    assert not stale_frame.exists()
